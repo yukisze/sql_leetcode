7f58702d4c60b9dc2dd9acfa7a9ffc17a4330d81
yukisze <50465879+yukisze@users.noreply.github.com>
2022-03-20 22:41:04 -0400

    Create THE PADS.sql

---
+++ THE PADS.sql
@@ -0,0 +1,5 @@
+-- Generate the following two result sets:
+
+-- Query an alphabetically ordered list of all names in OCCUPATIONS, 
+immediately followed by the first letter of each profession as a parenthetical (i.e.: enclosed in parentheses). 
+For example: AnActorName(A), ADoctorName(D), AProfessorName(P), and ASingerName(S)
